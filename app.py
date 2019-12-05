0bb1d67a6c10a1a6e42af6a68333dd384e15d266
Abinash Biswal <31894918+AbinashB1997@users.noreply.github.com>
2019-12-05 15:01:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,39 @@
-from flask import Flask, render_template,request,redirect,url_for
+from flask import Flask, url_for, render_template, redirect, request
 from pymongo import MongoClient as mongoDB
 import os
 
 app = Flask(__name__)
-title = "Team-Management"
-heading = "Team-Management App"
 
-#client = MongoClient("mongodb://127.0.0.1:27017")
+# client = mongoDB('http://localhost:27017/') //When running locally
+
 client = mongoDB(os.environ['host-0f-mongo'], 27017)
-db = client.mymongodb
-mycol = db.todo
 
-@app.route("/")
-@app.route("/list")
-def lists ():
-	return render_template('index.html', a1 = "active", mycol = mycol.find(), t = title, h = heading)
+db = client.mymongodbeid
+table = db.todo
+
+name = "Demo app"
+
+@app.route('/')
+@app.route('/list')
+def show_data():
+    return render_template('index.html', name = name, table = table.find())
 
 @app.route("/action1", methods=['POST'])
-def action1 ():
-	mycol.insert({ "name" : request.values.get("name"), "email" : request.values.get("email"), "date" : request.values.get("date"), "pr" : request.values.get("pr")})
-	return redirect("/list")
+def action1():
+    table.insert_one({ "name" : request.values.get("name"), "email" : request.values.get("email"), "date" : request.values.get("date"), "pr" : request.values.get("pr")})
+    return redirect("/list")
 
 @app.route("/action2", methods=['POST'])
-def action2 ():
-	data = {"email" : request.values.get("email")}
-	new_data = {'$set' : {"name" : request.values.get("name"), "email" : request.values.get("email"), "date" : request.values.get("date"), "pr" : request.values.get("pr")}}
-	mycol.update(data, new_data)
-	return redirect("/list")
+def action2():
+    old_data = {"email": request.values.get("email")}
+    new_data = { "name" : request.values.get("name"), "email" : request.values.get("email"), "date" : request.values.get("date"), "pr" : request.values.get("pr")}
+    table.update_one(old_data,{"$set": new_data})
+    return redirect("/list")
 
 @app.route("/action3", methods=['POST'])
-def action3 ():
-	email = request.values.get("email")
-	mycol.delete_one({"email" : email})
-	return redirect("/list")
+def action3():
+        table.delete_one({"email" : request.values.get("email")})
+        return redirect("/list")
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000', debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
